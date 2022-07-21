a26a842f5d41f7cfe4960d413e76225d37a77ba3
zrligon <109711510+zrligon@users.noreply.github.com>
2022-07-21 13:39:43 +0800

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,7 +2,8 @@
 import streamlit 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = my_fruit_list.set_index('Fruit')
+fruits_to_show = my_fruit_list.loc[fruits_selected]
+streamlit.dataframe(fruits_to_show)
 
 streamlit.title('My Parents New Healthy Dinner')
 streamlit.header('Breakfast Menu')
